eb8d7a34ec3e442a31a8250865ef26b62f543c51
Marvin <mk306@hdm-stuttgart.de>
2024-01-28 04:05:57 +0100

    added address prefix

--- main.tf
+++ main.tf
@@ -3,13 +3,24 @@ data "ibm_resource_group" "resource_group" {
 }
 
 resource "ibm_is_vpc" "vpc" {
-  name           = "vpc"
-  resource_group = data.ibm_resource_group.resource_group.id
+  name                      = "${var.prefix}-vpc"
+  address_prefix_management = "manual"
+  classic_access            = false
+  resource_group            = data.ibm_resource_group.resource_group.id
 }
 
-resource "ibm_is_subnet" "subnet_1" {
-  name            = "subnet-1"
+resource "ibm_is_subnet" "subnet" {
+  depends_on      = [ibm_is_vpc_address_prefix.address_prefix]
+  name            = "${var.prefix}-subnet"
   vpc             = ibm_is_vpc.vpc.id
   zone            = "${var.region}-1"
-  ipv4_cidr_block = "10.0.0.0/24"
+  ipv4_cidr_block = var.ipv4_cidr_block
+  resource_group  = data.ibm_resource_group.resource_group.id
+}
+
+resource "ibm_is_vpc_address_prefix" "address_prefix" {
+  name = "${var.prefix}-address-prefix"
+  vpc  = ibm_is_vpc.vpc.id
+  cidr = var.ipv4_cidr_block
+  zone = "${var.region}-1"
 }
--- variables.tf
+++ variables.tf
@@ -2,8 +2,10 @@
 # Sensitive Account Variables
 ##############################################################################
 
-variable ibmcloud_api_key {
+variable "ibmcloud_api_key" {
   description = "The IBM Cloud platform API key needed to deploy IAM enabled resources"
+  type        = string
+  sensitive   = true
 }
 
 
@@ -11,10 +13,31 @@ variable ibmcloud_api_key {
 # Account Variables
 ##############################################################################
 
-variable region {
-  description = "IBM Cloud region where all resources will be deployed"
+variable "prefix" {
+  description = "A unique identifier need to provision resources. Must begin with a letter."
+  type        = string
+  default     = "stock-app"
+
+  validation {
+    error_message = "Unique ID must begin and end with a letter and contain only letters, numbers, and - characters."
+    condition     = can(regex("^([a-z]|[a-z][-a-z0-9]*[a-z0-9])$", var.prefix))
+  }
 }
 
-variable resource_group {
-  description = "Name of resource group to provision resources"
+variable "region" {
+  description = "IBM Cloud region where all resources will be deployed."
+  type        = string
+  default     = "eu-de"
+}
+
+variable "resource_group" {
+  description = "Name of resource group to provision resources."
+  type        = string
+  default     = "Default"
+}
+
+variable "ipv4_cidr_block" {
+  description = "The IPv4 range of the subnet."
+  type        = string
+  default     = "10.0.0.0/24"
 }
