8be50b571b8c32e08a6c8a88c8e4668cce95927b
Marvin <mk306@hdm-stuttgart.de>
2024-02-23 19:43:03 +0100

    output fixes web and python server

--- modules/python-server/outputs.tf
+++ modules/python-server/outputs.tf
@@ -4,5 +4,5 @@
 
 output "python_server_ip" {
   description = "The IP to connect with the python server"
-  value       = kubernetes_service.python_server_loadbalancer.status.0.load_balancer.0.ingress.0.hostname
+  value       = kubernetes_service.python_server_service.status.0.load_balancer.0.ingress.0.hostname
 }
--- modules/web-server/outputs.tf
+++ modules/web-server/outputs.tf
@@ -4,5 +4,5 @@
 
 output "web_server_ip" {
   description = "The IP to connect with the web server"
-  value       = kubernetes_service.web_server_loadbalancer.status.0.load_balancer.0.ingress.0.hostname
+  value       = kubernetes_service.web_server_service.status.0.load_balancer.0.ingress.0.hostname
 }
